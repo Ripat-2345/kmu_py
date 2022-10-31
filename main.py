2e544ed831eaa81a6b6ad9fd5c3779b78909f0bc
Ripat-2345 <ripatalsafar@gmail.com>
2022-10-31 23:21:17 +0700

    v.1.0.0 done

--- main.py
+++ main.py
@@ -1,5 +1,37 @@
 # Konversi Mata Uang App
 
-# nama saya fahri andrianto
+# Mata Uang : IND, US, JPY, THA, SBP
+list_mata_uang = ['IND', 'US', 'JPY', 'THA', 'TUR']
 
-# kuraimos
+while True:
+    print(35*"=")
+    print(f"Mata Uang Yang Tersedia\n{list_mata_uang}")
+    print(10*"=","PILIHAN MENU APLIKASI",10*"=")
+    print("1.Konversi keseluruh mata uang yang tersedia")
+    print("2.Konversi ke salah satu mata uang")
+    pilihan = int(input("=>"))
+    
+    # Operasi Pilihan Pertama
+    if(pilihan == 1):
+        mata_uang_sekarang = int(input("Masukkan Mata Uang Sekarang : Rp."))
+        dollarKeRupiah = mata_uang_sekarang // 14000
+        print(f"Hasil Konversi\n=>(US)Dollar = {dollarKeRupiah}\n=>(JPY)Jepang = {dollarKeRupiah}")
+    # Operasi Pilihan Kedua
+    elif(pilihan == 2):
+        kode_mata_uang = input("Masukkan Kode Mata Uang : ")
+        # Konversi ke US
+        if(kode_mata_uang == 'US'):
+            mata_uang_sekarang = int(input("Masukkan Mata Uang Sekarang : Rp."))
+            dollarKeRupiah = mata_uang_sekarang / 14000
+            print(f"Hasil Konversi {dollarKeRupiah} Dollar")
+        
+        # Konversi ke JPY
+
+        # Konversi ke THA
+
+        # Konversi ke TUR
+
+    print(35*"=")
+    isSelesai = input("Apakah Sudah Selesai\n1.yes\n2.no\n=>")
+    if int(isSelesai) == 1:
+        break
