146a4d2ef8c85f29f66e8411c80d5678183e14d5
Ripat-2345 <ripatalsafar@gmail.com>
2022-11-08 19:30:56 +0700

    Rapihkan Koding

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Konversi Mata Uang App
 
-# Mata Uang : IND, US, JPY, THA, SBP
+# Mata Uang : 'IND', 'US', 'JPY', 'THA', 'TUR', 'CNY', 'SGD', 'KOR'
 listMataUang = ['IND', 'US', 'JPY', 'THA', 'TUR', 'CNY', 'SGD', 'KOR']
 
 while True:
@@ -17,14 +17,15 @@ while True:
     print("3.Keluar")
     print(47*"=")
     pilihan = int(input("=>"))
-    
+# * ============================================================================
     # Operasi Pilihan Pertama
     if(pilihan == 1):
         kodeNegara = input("Masukkan Kode Negara :")
+        # * ==================================================
         if kodeNegara == 'IND' or kodeNegara == 'ind':
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             INDkeUS = jumlahUang // 14000
-            print(f"({listMataUang[1]})\t=> ${INDkeUS:.2f}")
+            print(f"({listMataUang[1]})\t=> US${INDkeUS:.2f}")
 
             INDkeJPY = jumlahUang / 105.17
             print(f"({listMataUang[2]})\t=> ¥{INDkeJPY:.2f}")
@@ -40,7 +41,7 @@ while True:
 
             INDkeSGD = jumlahUang / 11043
             print(f"({listMataUang[6]})\t=> S${INDkeSGD:.2f}")
-
+        # * ==================================================
         elif kodeNegara == 'US' or kodeNegara == 'us':
             jumlahUang = int(input("Masukkan Jumlah Uang : $"))
             USkeIND = jumlahUang * 14000
@@ -60,13 +61,15 @@ while True:
 
             USkeSGD = jumlahUang / 11043
             print(f"({listMataUang[6]})\t=> S${USkeSGD:.2f}")
-
+        # * ==================================================
         else:
             print(47*"=")
             print("Kode Negara Tidak Ada Silahkan Pilih Yang Ada")
+# * ============================================================================
     # Operasi Pilihan Kedua
     elif(pilihan == 2):
         kodeNegaraTujuan = input("Masukkan Kode Negara Tujuan : ")
+        # * ==================================================
         # Konversi ke US
         if(kodeNegaraTujuan == 'US'):
             isLanjut = True
@@ -79,57 +82,94 @@ while True:
                     USkeIND = jumlahUang / 14000
                     print(f"=> ${USkeIND:.2f}")
                     isLanjut = False
+                elif kodeNegaraAsal == 'JPY' or kodeNegaraAsal == 'jpy':
+                    jumlahUang = int(input("Masukkan Jumlah Uang : ¥"))
+                    USkeJPY = jumlahUang / 14000
+                    print(f"=> ${USkeJPY:.2f}")
+                    isLanjut = False
+                elif kodeNegaraAsal == 'THA' or kodeNegaraAsal == 'tha':
+                    jumlahUang = int(input("Masukkan Jumlah Uang : ฿"))
+                    USkeTHA = jumlahUang / 14000
+                    print(f"=> ${USkeTHA:.2f}")
+                    isLanjut = False
+                elif kodeNegaraAsal == 'TUR' or kodeNegaraAsal == 'tur':
+                    jumlahUang = int(input("Masukkan Jumlah Uang : ₺"))
+                    USkeTUR = jumlahUang / 14000
+                    print(f"=> ${USkeTUR:.2f}")
+                    isLanjut = False
+                elif kodeNegaraAsal == 'CNY' or kodeNegaraAsal == 'CNY':
+                    jumlahUang = int(input("Masukkan Jumlah Uang : ¥"))
+                    USkeCNY = jumlahUang / 14000
+                    print(f"=> ${USkeCNY:.2f}")
+                    isLanjut = False
+                elif kodeNegaraAsal == 'SGD' or kodeNegaraAsal == 'sgd':
+                    jumlahUang = int(input("Masukkan Jumlah Uang : S$"))
+                    USkeSGD = jumlahUang / 14000
+                    print(f"=> ${USkeSGD:.2f}")
+                    isLanjut = False
                 else:
                     print(47*"=")
                     print("Maaf Kode Negara Anda Tidak Tersedia\nAnda Dapat Mencoba Kode Yang Tersedia\nSilahkan Ketik 'BACK' Untuk Keluar Dari Menu 2")
                     print(47*"=")
-        
+        # * ==================================================
         # Konversi ke JPY
         elif(kodeNegaraTujuan == 'JPY'):
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             JPYKeIND = jumlahUang / 105.17
-            print(f"=> ¥{JPYKeIND:.2f}") 
+            print(f"=> ¥{JPYKeIND:.2f}")
+        # * ==================================================
         # Konversi ke THA
         elif(kodeNegaraTujuan == 'THA'):
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             THAkeIND = jumlahUang / 409.76
-            print(f"=> ฿{THAkeIND:.2f}") 
+            print(f"=> ฿{THAkeIND:.2f}")
+        # * ==================================================
         # Konversi ke TUR
         elif(kodeNegaraTujuan == 'TUR'):
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             TURkeIND = jumlahUang / 841.44
             print(f"=> ₺{TURkeIND:.2f}")
-        
+        # * ==================================================
         # Konversi ke CNY
         elif(kodeNegaraTujuan == 'CNY'):
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             CNYkeIND = jumlahUang / 2167.98
             print(f"=> ¥{CNYkeIND:.2f}")
-
+        # * ==================================================
         # Konversi ke SGD
         elif(kodeNegaraTujuan == 'SGD'):
             jumlahUang = int(input("Masukkan Jumlah Uang : RP"))
             SGDkeIND = jumlahUang / 11043
             print(f"=> S${SGDkeIND:.2f}")
-        
+        # * ==================================================
         # Konversi Ke KOR
+
+# * ============================================================================
     # Operasi Pilihan Ketiga
     elif(pilihan == 3):
         print(47*"=")
         print("Terima Kasih Telah Menggunakan Aplikasi Kami :)")
         print(47*"=")
         break
+# * ============================================================================
     # Operasi Pilihan Tidak Tersedia
     else:
         print(47*"=")
         print("Pilihan Tidak Tersedia Silahkan Pilih Yang Lain")
-
+# * ============================================================================
     print(47*"=")
-    isLanjut = input("Lanjut Konversi?(yes/no)\n=>")
-    if isLanjut == 'yes' or isLanjut == 'YES':
-        continue
-    else:
-        print(47*"=")
-        print("Terima Kasih Telah Menggunakan Aplikasi Kami :)")
-        print(47*"=")
-        break
+    lanjut = True
+    while lanjut:
+        isLanjut = input("Lanjut Konversi?(yes/no)\n=>")
+        if isLanjut == 'yes' or isLanjut == 'YES':
+            lanjut = False
+        elif isLanjut == 'no' or isLanjut == 'NO':
+            print(47*"=")
+            print("Terima Kasih Telah Menggunakan Aplikasi Kami :)")
+            print(47*"=")
+            exit()
+        else:
+            print(47*"=")
+            print("Pilihan Keyword Tidak Ada!")
+            print(47*"=")
+            continue
